e89c62c3cd96e8d5310ce26881302af732865876
thexiv <inland14@live.com>
2018-12-12 12:42:16 -0500

    Real-life Spheres not that thing that was here

--- fms.cpp
+++ fms.cpp
@@ -1,6 +1,6 @@
 /*
     FMS - Field Metric Scalar for Circles and Spheres to Squares and Cubes
-    Copyright (C) 2018  Anthony David Pulse, Jr.
+    Copyright (C) 2018  Anthony David Pulse, Jr. - inland14@live.com
   
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -45,68 +45,104 @@ int main(int argc, char * argv[]) {
 	z = 0;
 	int j = 0, i = 0;
 	j = 0;
-	if (pow(h,2) - pow(((h/pi)*x),2)*pi > 0)
-		while (pow((h/pi)*x + z,2)*pi - pow(h,2) > 0)
+
+
+	int a = 0;
+	float f = 0, b = 0;
+	int c = 0;
+
+	if (pow(h,2) - pow((h*0.56418957569775374239),2)*pi > 0)
+		while (pow((h*0.56418957569775374239) + z,2)*pi - pow(h,2) > 0)
 			z += 1;
 	else
-		while (pow((h/pi)*x + z,2)*pi - pow(h,2) < 0)
+		while (pow((h*0.56418957569775374239) + z,2)*pi - pow(h,2) < 0)
 			z -= 1;
-	if (pow(h,2) - pow((h/pi)*x+z,2)*pi > 0)
-		while (pow((h/pi)*x + z,2)*pi - pow(h,2) > 0)
+	if (pow(h,2) - pow((h*0.56418957569775374239)+z,2)*pi > 0)
+		while (pow((h*0.56418957569775374239) + z,2)*pi - pow(h,2) > 0)
 			z -= 0.00000001;
 	else
-		while (pow((h/pi)*x + z,2)*pi - pow(h,2) < 0)
+		while (pow((h*0.56418957569775374239) + z,2)*pi - pow(h,2) < 0)
 			z += 0.00000001;
+
 	cout << "\tx = sqrt(Pi)\t";
-	cout << std::fixed << std::setprecision(20) << "\tpow((h/pi)*x,2)*pi = " << pow((h/pi)*x,2)*pi << "\tSquare Area: " << pow(h,2) << endl;
+	cout << std::fixed << std::setprecision(20) << "\tpow((h*0.56418957569775374239),2)*pi = " << pow((h*0.56418957569775374239),2)*pi << "\tSquare Area: " << pow(h,2) << endl;
 	cout << "\t\tCircle (e-20)\t\tRadius (e-20)\t\t\tIntegration (e-30)\t\t\t\tDifferential (e-30)" << endl;
 
 	float s = 0, t = 0;
 	w = z;
 
-//	if (pow(h,2) - pow(((h/pi)*x+w),2)*pi > 0.0000009)
-		while (pow(h,2) - pow((h/pi)*x+w + (0.00000000001*s),2)*pi > 0.000009)
+//	if (pow(h,2) - pow(((h*0.56418957569775374239)+w),2)*pi > 0.0000009)
+		while (pow(h,2) - pow((h*0.56418957569775374239)+w + (0.00000000001*s),2)*pi > 0.00000009)
 			s += 1;
 //	else
-		while (pow(h,2) - pow((h/pi)*x+w + (0.00000000001*s),2)*pi < 0.000009)
+		while (pow(h,2) - pow((h*0.56418957569775374239)+w + (0.00000000001*s),2)*pi < 0.00000009)
 			s -= 1;
 	s *= 0.00000000001;
 	j = 0;
-//	if (pow(h,2) - pow(((h/pi)*x+w+s),2)*pi > 0.0000000009)
-		while (pow(h,2) - pow(((h/pi)*x+w+s + (0.00000000000001*j)),2)*pi > 0.0000000009)
-			j += 10;
+//	if (pow(h,2) - pow(((h*0.56418957569775374239)+w+s),2)*pi > 0.0000000009)
+		while (pow(h,2) - pow(((h*0.56418957569775374239)+w+s + (0.00000000000001*j)),2)*pi > 0.00000000000001)
+			j += 1;
 //	else
-		while (pow(h,2) - pow(((h/pi)*x+w+s + (0.00000000000001*j)),2)*pi < 0.0000000009)
-			j -= 10;
-	int c = j;
+		while (pow(h,2) - pow(((h*0.56418957569775374239)+w+s + (0.00000000000001*j)),2)*pi < 0.00000000000001)
+			j -= 1;
+
+	float g = j * 0.00000000000001;
+	j = 0;
+		while (pow(h,2) - pow(((h*0.56418957569775374239)+w+s +g+ (0.000000000000000001*(j))),2)*pi > 0.00000000000000001)
+			j += 1;
+		while (pow(h,2) - pow(((h*0.56418957569775374239)+w+s +g+ (0.000000000000000001*(j))),2)*pi < 0.00000000000000001)
+			j -= 1;
+
+	c = j;
 	while (j > c - 20) {
-		printf("%d\t%.20f\t%.20f\t%.30f\t%.30f\n",j,pow((h/pi)*x+ w+s+(0.00000000000001*j),2)*pi, (h/pi)*x+w+s+(0.00000000000001*j), pow(h,2) - pow((h/pi)*x+w+s+(0.00000000000001*j),2)*pi - pow((h/pi)*x+w+s+(0.00000000000001*j),2)*pi + pow(h,2), pow((h/pi)*x+w+s+(0.00000000000001*j),2)*pi - pow(h,2));
+		printf("%d\t%.20f\t%.20f\t%.30f\t%.30f\n",j,pow((h*0.56418957569775374239)+ w+s+g+(0.000000000000000001*(5+j)),2)*pi, (h*0.56418957569775374239)+w+s+g+(0.000000000000000001*(5+j)), pow(h,2) - pow((h*0.56418957569775374239)+w+s+g+(0.000000000000000001*(5+j)),2)*pi - pow((h*0.56418957569775374239)+w+s+g+(0.000000000000000001*(5+j)),2)*pi + pow(h,2), pow((h*0.56418957569775374239)+w+s+g+(0.000000000000000001*(5+j)),2)*pi - pow(h,2));
 		j--;
 	}
-
-
-	int a = 0;
-	// Sphere
-	float b = 0;
-	float f = 0;
-	while (a < 23) {
-		while (pow((h*0.6827840569218967)+b+f,3)*pi < pow(h,3))
-			f += (1*(100/pow(10,a)));
-		while (pow((h*0.6827840569218967)+b+f,3)*pi > pow(h,3))
-			f -= (1*(100/pow(10,a)));
-		b += f;
-		f = 0;
-		a++;
-	}
-
-	cout << "\n\tpow((h*0.6827840569218967),3)*pi = " << pow(((h*0.6827840569218967)+z),3)*pi << "\t\tCubic Area: " << pow(h,3) << endl;
+	cout << "\n\tpow(((h*0.28209478784887687119)+(b+f))/10*(h),2)*pi*4 = " << pow(((h*0.28209478784887687119)+(b+f))/10*(h),2)*pi*4 << "\t\tCubic Area: " << pow(h,3) << endl;
 	cout << "\t\tSphere\t\t\t\tRadius\t\t\tIntegration\t\t\tDifferential" << endl;
 	float p = b;
+	z = 0;
+	if (pow(h,2) - pow((h*0.28209478784887687119)+(b+f),2)*pi*4 > 0)
+		while (pow((h*0.28209478784887687119)+(b+f) + z,2)*pi*4 - pow(h,2) > 0)
+			z += 1;
+	else
+		while (pow((h*0.28209478784887687119)+(b+f) + z,2)*pi*4 - pow(h,2) < 0)
+			z -= 1;
+	if (pow(h,2) - pow((h*0.28209478784887687119)+(b+f)+z,2)*pi*4 > 0)
+		while (pow((h*0.28209478784887687119)+(b+f) + z,2)*pi*4 - pow(h,2) > 0)
+			z -= 0.00000001;
+	else
+		while (pow((h*0.28209478784887687119)+(b+f) + z,2)*pi*4 - pow(h,2) < 0)
+			z += 0.00000001;
+	s = 0, t = 0;
+	w = z;
 
-	j = 5;
-//	while (j > 0) {
-		printf("%d\t%.20f\t%.20f\t%.30f\t%.30f\n",j,pow((h*0.6827840569218967)+ p,3)*pi, (h*0.6827840569218967)+ p, pow(h,3) - pow((h*0.6827840569218967)+ p,3)*pi - pow((h*0.6827840569218967)+ p,3)*pi + pow(h,3), pow((h*0.6827840569218967)+ p,3)*pi - pow(h,3));
-//		j--;
-//	}
+//	if (pow(h,2) - pow(((h*0.28209478784887687119)+(b+f)+w),2)*pi*4 > 0.0000009)
+		while (pow(h,2) - pow((h*0.28209478784887687119)+(b+f)+w + (0.00000000001*s),2)*pi*4 > 0.00000009)
+			s += 1;
+//	else
+		while (pow(h,2) - pow((h*0.28209478784887687119)+(b+f)+w + (0.00000000001*s),2)*pi*4 < 0.00000009)
+			s -= 1;
+	s *= 0.00000000001;
+	j = 0;
+//	if (pow(h,2) - pow(((h*0.28209478784887687119)+(b+f)+w+s),2)*pi*4 > 0.0000000009)
+		while (pow(h,2) - pow(((h*0.28209478784887687119)+(b+f)+w+s + (0.00000000000001*j)),2)*pi*4 > 0.00000000000001)
+			j += 1;
+//	else
+		while (pow(h,2) - pow(((h*0.28209478784887687119)+(b+f)+w+s + (0.00000000000001*j)),2)*pi*4 < 0.00000000000001)
+			j -= 1;
+
+	g = j * 0.00000000000001;
+	j = 0;
+		while (pow(h,2) - pow(((h*0.28209478784887687119)+(b+f)+w+s +g+ (0.000000000000000001*(j))),2)*pi*4 > 0.00000000000000001)
+			j += 1;
+		while (pow(h,2) - pow(((h*0.28209478784887687119)+(b+f)+w+s +g+ (0.000000000000000001*(j))),2)*pi*4 < 0.00000000000000001)
+			j -= 1;
+
+	c = j;
+	while (j > c - 20) {
+		printf("%d\t%.20f\t%.20f\t%.30f\t%.30f\n",j,pow((h*0.28209478784887687119)+(b+f)+ w+s+g+(0.000000000000000001*(5+j)),2)*pi*4, (h*0.28209478784887687119)+(b+f)+w+s+g+(0.000000000000000001*(5+j)), pow(h,2) - pow((h*0.28209478784887687119)+(b+f)+w+s+g+(0.000000000000000001*(5+j)),2)*pi*4 - pow((h*0.28209478784887687119)+(b+f)+w+s+g+(0.000000000000000001*(5+j)),2)*pi*4 + pow(h,2), pow((h*0.28209478784887687119)+(b+f)+w+s+g+(0.000000000000000001*(5+j)),2)*pi*4 - pow(h,2));
+		j--;
+	}
 	return 1;
 }
