d0c86c7566ef9220b21c483edc3d31f3ffc103ed
wise-penny <inland14@live.com>
2021-04-20 03:02:39 -0400

    Update fms.cpp

--- fms.cpp
+++ fms.cpp
@@ -1,17 +1,17 @@
 /*
     FMS - Field Metric Scalar for Circles and Spheres to Squares and Cubes
-    Copyright (C) 2018  Anthony David Pulse, Jr. - inland14@live.com
-  
+    Copyright (C) 2021  Anthony David Pulse, Jr. - inland14@live.com
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-  
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-  
+
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
@@ -52,11 +52,10 @@ int main(int argc, char * argv[]) {
 	float f = 0, b = 0;
 	int c = 0;
 
-	cout << "\tx = sqrt(Pi)\t";
 	cout << std::fixed << std::setprecision(20) << "\tpow((h*0.56418957569775374239),2)*pi = " << pow((h*0.56418957569775374239),2)*pi << "\tSquare Area: " << pow(h,2) << endl;
-	cout << "\t\tCircle (e-20)\t\tRadius (e-20)\n\r" << endl;
+	cout << "\t\t\tArea (e-20)\t\t\t\tRadius (e-20)\t\t\t\tDiameter (e-20)\t\t\tCircumference (e-20)\t\t\t\tRatio (e-20)\n\r" << endl;
 
-	printf("\t\t%.20f\t%.20f\n",pow((h*0.56418957569775374239),2)*pi, (h*0.56418957569775374239));//, (0.33333332405748857138 * pow(h,3)), 1.9999999443449314283 * h);
-	cout << "\n\tpow(h*0.56418957569775374239),2)*pi*h = " << (pow(h*0.56418957569775374239,2)*pi*h) << "\t\tCubic Area: " << pow(h,3) << endl;
+	printf("\t\t%.20f\t%.20f\t\t%.20f\t\t%.20f\t\t%.20f\t%.20f",pow((h*0.56418957569775374239),2)*pi,(h*0.56418957569775374239),(h*0.56418957569775374239*2),2*pi*(h*0.56418957569775374239),(2*pi*(h*0.56418957569775374239))/(2*(h*0.56418957569775374239)));
+	cout << "\n\n\n\tpow(h*0.56418957569775374239,2)*pi*h = " << (pow(h*0.56418957569775374239,2)*pi*h) << "\t\tCubic Area: " << pow(h,3) << endl;
 	return 1;
 }
