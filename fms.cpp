2176943b759e427b7b171a74266bc372983fabd4
wise-penny <inland14@live.com>
2022-07-14 23:12:03 -0400

    Update fms.cpp

--- fms.cpp
+++ fms.cpp
@@ -52,10 +52,10 @@ int main(int argc, char * argv[]) {
 	float f = 0, b = 0;
 	int c = 0;
 
-	cout << std::fixed << std::setprecision(20) << "\tpow((h*0.56418957569775374239),2)*pi = " << pow((h*0.56418957569775374239),2)*pi << "\tSquare Area: " << pow(h,2) << endl;
+	cout << std::fixed << std::setprecision(20) << "\tpow((h*0.56418959),2)*pi = " << pow((h*0.56418959),2)*pi << "\tSquare Area: " << pow(h,2) << endl;
 	cout << "\t\t\tArea (e-20)\t\t\t\tRadius (e-20)\t\t\t\tDiameter (e-20)\t\t\tCircumference (e-20)\t\t\t\tRatio (e-20)\n\r" << endl;
 
-	printf("\t\t%.20f\t%.20f\t\t%.20f\t\t%.20f\t\t%.20f\t%.20f",pow((h*0.56418957569775374239),2)*pi,(h*0.56418957569775374239),(h*0.56418957569775374239*2),2*pi*(h*0.56418957569775374239),(2*pi*(h*0.56418957569775374239))/(2*(h*0.56418957569775374239)));
-	cout << "\n\n\n\tpow(h*0.56418957569775374239,2)*pi*h = " << (pow(h*0.56418957569775374239,2)*pi*h) << "\t\tCubic Area: " << pow(h,3) << endl;
+	printf("\t\t%.20f\t%.20f\t\t%.20f\t\t%.20f\t\t%.20f\t%.20f",pow((h*0.56418959),2)*pi,(h*0.56418959),(h*0.56418959*2),2*pi*(h*0.56418959),(2*pi*(h*0.56418959))/(2*(h*0.56418959)));
+	cout << "\n\n\n\tpow(h*0.56418957569775374239,2)*pi*h = " << (pow(h*0.56418959,2)*pi*h) << "\t\tCubic Area: " << pow(h,3) << endl;
 	return 1;
 }
